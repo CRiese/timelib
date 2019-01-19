4292075d8a51fbd2d4162280f108d79d944e5f96
CRiese <catharinariese.cr@gmail.com>
2019-01-20 00:26:57 +0100

    Add function

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /**
  * Author : Catharina Riese
  * Date : 18.12.2018
- * Vom eingegebenem Datum soll ermittelt werden welcher Tag, dass im Jahr ist
+ * Vom eingegebenem Datum soll ermittelt werden, welcher Tag das im Jahr ist
  **/
 
 #include <stdio.h>
@@ -49,23 +49,37 @@
  * Einlesen von 3 Ganzzahlenwerte für Tag, Monat, Jahr
  *
  **/
- int input_date(int day, int month, int year)
- {
+ //int input_date(int day, int month, int year)
+ //{
 
- }
+ //}
 
  /**
- * Bestimmung für den gegebenen Monat eines gegebenen Jahres,
- * wie viele Tage der Moant hat
+ * Die Funktion bestimmt für einen gegebenen Monat
+ * eines gegebenen Jahres, wie viele Tage der Monat hat.
+ * Der Wert des Monats muss zwischen 1 und 12 liegen.
+ * Schaltjahre werden berücksichtigt.
  **/
  int get_days_for_month(int month, int year)
  {
-    int array[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    if (isLeapyear(year) == 0)
+    {
+        int array[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        return array[month-1];
+    }
+
+    if (isLeapyear(year) == 1)
+    {
+        int array[] = {31,29,31,30,31,30,31,31,30,31,30,31};
+        return array[month-1];
+    }
+
+
  }
 
  /**
  * überprüft ob das Datum gültig ist.
- * Zeitraum von 1.1.1582 und 31.12.2400
+ * Im Zeitraum von 1.1.1582 und 31.12.2400
  **/
  int exists_date(int day, int month, int year)
  {
@@ -94,20 +108,34 @@
  }
 
 /**
- * berechent für das gegebene Datum die Nummer des Tages,
+ * berechnet für das gegebene Datum die Nummer des Tages,
  * von Jahresbeginn an gezählt
  **/
 int day_of_the_year(int day, int month, int year)
 {
+    int exists = exists_date(day,month,year);
+    int i = 1, alldays = 0;
+    if (exists ==1)
+    {
+        while(i<month)
+        {
+            alldays+= get_days_for_month(month,year);
+            month--;
+        }
 
-
-    return 0;
+        alldays+= day;
+        return alldays;
+    }
+    else
+    {
+        return -1;
+    }
 }
 
 int main()
 {
-    printf("Tag des Jahres: %i\n", day_of_the_year(31, 12, 2018));
 
-    printf("Tag ist korrekt: %i\n", exists_date(31, 12, 2018));
+    printf("Tag des Jahres: %i\n", day_of_the_year(1, 1, 2020));
+
     return 0;
 }
